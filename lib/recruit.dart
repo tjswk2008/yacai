0cb7adf51c0749aaa612aab77f7f60ccd3f2519d
Liao Hui <liaohui@ztgame.com>
2019-02-11 18:46:50 +0800

    add recruit

---
+++ lib/recruit.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/app/component/icon_tab.dart';
+import 'package:flutter_app/app/view/jobs_view.dart';
+import 'package:flutter_app/app/view/message_view.dart';
+import 'package:flutter_app/app/view/mine_view.dart';
+
+const double _kTabTextSize = 11.0;
+const int INDEX_JOB = 0;
+const int INDEX_COMPANY = 1;
+const int INDEX_MESSAGE = 2;
+const int INDEX_MINE = 3;
+Color _kPrimaryColor = new Color.fromARGB(255, 0, 215, 198);
+
+class Recruit extends StatefulWidget {
+  @override
+  HomeState createState() => new HomeState();
+}
+
+class HomeState extends State<Recruit> with SingleTickerProviderStateMixin {
+  int _currentIndex = 0;
+  TabController _controller;
+  VoidCallback onChanged;
+  Widget mine = new MineTab();
+
+  @override
+  void initState() {
+    super.initState();
+    _controller =
+        new TabController(initialIndex: _currentIndex, length: 4, vsync: this);
+    onChanged = () {
+      setState(() {
+        _currentIndex = this._controller.index;
+      });
+    };
+
+    _controller.addListener(onChanged);
+  }
+
+  @override
+  void dispose() {
+    _controller.removeListener(onChanged);
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: new TabBarView(
+        children: <Widget>[
+          new JobsTab(1, '全职'),
+          new JobsTab(2, '兼职/实习'),
+          new MessageTab('交流'),
+          mine
+        ],
+        controller: _controller,
+      ),
+      bottomNavigationBar: new Material(
+        color: Colors.white,
+        child: new TabBar(
+          controller: _controller,
+          indicatorSize: TabBarIndicatorSize.label,
+          labelStyle: new TextStyle(fontSize: _kTabTextSize),
+          tabs: <IconTab>[
+            new IconTab(
+                icon: _currentIndex == INDEX_JOB
+                    ? "assets/images/ic_main_tab_company_pre.png"
+                    : "assets/images/ic_main_tab_company_nor.png",
+                text: "全职",
+                color: _currentIndex == INDEX_JOB
+                    ? _kPrimaryColor
+                    : Colors.grey[900]),
+            new IconTab(
+                icon: _currentIndex == INDEX_COMPANY
+                    ? "assets/images/ic_main_tab_contacts_pre.png"
+                    : "assets/images/ic_main_tab_contacts_nor.png",
+                text: "兼职/实习",
+                color: _currentIndex == INDEX_COMPANY
+                    ? _kPrimaryColor
+                    : Colors.grey[900]),
+            new IconTab(
+                icon: _currentIndex == INDEX_MESSAGE
+                    ? "assets/images/ic_main_tab_find_pre.png"
+                    : "assets/images/ic_main_tab_find_nor.png",
+                text: "交流",
+                color: _currentIndex == INDEX_MESSAGE
+                    ? _kPrimaryColor
+                    : Colors.grey[900]),
+            new IconTab(
+                icon: _currentIndex == INDEX_MINE
+                    ? "assets/images/ic_main_tab_my_pre.png"
+                    : "assets/images/ic_main_tab_my_nor.png",
+                text: "我的",
+                color: (_currentIndex == INDEX_MINE)
+                    ? _kPrimaryColor
+                    : Colors.grey[900]),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(new MaterialApp(
+      title: "丫财",
+      theme: new ThemeData(
+        primaryIconTheme: const IconThemeData(color: Colors.white),
+        brightness: Brightness.light,
+        primaryColor: _kPrimaryColor,
+        accentColor: Colors.cyan[300],
+      ),
+      home: new Recruit()
+  ));
+}
--- lib/splash.dart
+++ lib/splash.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_app/home.dart';
+import 'package:flutter_app/recruit.dart';
 
 class SplashPage extends StatefulWidget {
   @override
@@ -15,20 +16,20 @@ class SplashState extends State<SplashPage> {
   @override
   void initState() {
     super.initState();
-    _t = new Timer(const Duration(milliseconds: 1500), () {
-      try {
-        Navigator.of(context).pushAndRemoveUntil(new MaterialPageRoute(
-            builder: (BuildContext context) => new BossApp()), (
-            Route route) => route == null);
-      } catch (e) {
+    // _t = new Timer(const Duration(milliseconds: 1500), () {
+    //   try {
+    //     Navigator.of(context).pushAndRemoveUntil(new MaterialPageRoute(
+    //         builder: (BuildContext context) => new BossApp()), (
+    //         Route route) => route == null);
+    //   } catch (e) {
 
-      }
-    });
+    //   }
+    // });
   }
 
   @override
   void dispose() {
-    _t.cancel();
+    // _t.cancel();
     super.dispose();
   }
 
@@ -36,18 +37,62 @@ class SplashState extends State<SplashPage> {
   Widget build(BuildContext context) {
     return new Material(
       color: new Color.fromARGB(255, 0, 215, 198),
-      child: new Padding(
-        padding: const EdgeInsets.only(
-          top: 150.0,
-        ),
-        child: new Column(
-          children: <Widget>[
-            new Text("丫财",
-              style: new TextStyle(color: Colors.white,
-                  fontSize: 50.0,
-                  fontWeight: FontWeight.bold),),
-          ],
-        ),
+      child: new Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          new Text("丫财",
+            style: new TextStyle(
+              color: Colors.white,
+              fontSize: 50.0,
+              fontWeight: FontWeight.bold
+            ),
+          ),
+          new Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              new Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: new InkWell(
+                  onTap: () {
+                    Navigator.of(context).pushAndRemoveUntil(new MaterialPageRoute(
+                      builder: (BuildContext context) => new Recruit()), (
+                      Route route) => route == null);
+                  },
+                  child: new Container(
+                    height: 30.0,
+                    decoration: new BoxDecoration(
+                      border: new Border.all(color: Colors.orange[100]),
+                      borderRadius: new BorderRadius.all(new Radius.circular(5.0))
+                    ),
+                    child: new Center(
+                      child: new Text('我是招聘者', style: new TextStyle(color: Colors.white, fontSize: 14.0),),
+                    ),
+                  ),
+                ),
+              ),
+              new Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: new InkWell(
+                  onTap: () {
+                    Navigator.of(context).pushAndRemoveUntil(new MaterialPageRoute(
+                      builder: (BuildContext context) => new BossApp()), (
+                      Route route) => route == null);
+                  },
+                  child: new Container(
+                    height: 30.0,
+                    decoration: new BoxDecoration(
+                      border: new Border.all(color: Colors.orange[200]),
+                      borderRadius: new BorderRadius.all(new Radius.circular(5.0))
+                    ),
+                    child: new Center(
+                      child: new Text('我是求职者', style: new TextStyle(color: Colors.white, fontSize: 14.0),),
+                    ),
+                  ),
+                ),
+              )
+            ]
+          )
+        ],
       ),
     );
   }
