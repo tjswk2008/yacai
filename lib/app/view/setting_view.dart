744224502371a9daf047eef8fbc8d819545697ed
Liao Hui <liaohui@ztgame.com>
2019-04-28 18:24:13 +0800

    update addr

--- lib/app/view/setting_view.dart
+++ lib/app/view/setting_view.dart
@@ -383,6 +383,7 @@ class SettingViewState extends State<SettingView> {
   Future<void> executeDownload() async {
     final path = await _apkLocalPath;
     String downLoadUrl = Platform.isAndroid ? 'http://192.168.140.56:8080/public' : 'http://192.168.2.101:8080/public';
+    // String downLoadUrl = 'http://192.168.2.101:8080/public';
     //下载
     final taskId = await FlutterDownloader.enqueue(
         url: downLoadUrl + '/app-release.apk',
