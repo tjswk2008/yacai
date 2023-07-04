6a3be76448fcaf2bc2301b920c8f1b3a14850e61
liaohui <tjswk2008@163.com>
2023-07-04 16:06:48 +0800

    更新依赖包

--- android/app/src/main/kotlin/com/heruijun/flutterapp/MainActivity.kt
+++ android/app/src/main/kotlin/com/heruijun/flutterapp/MainActivity.kt
@@ -4,10 +4,10 @@ import android.os.Bundle
 
 import io.flutter.app.FlutterActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
-import io.flutter.embedding.engine.FlutterEngine
 
 class MainActivity(): FlutterActivity() {
-  override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
-    GeneratedPluginRegistrant.registerWith(flutterEngine) // add this line
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
   }
 }
